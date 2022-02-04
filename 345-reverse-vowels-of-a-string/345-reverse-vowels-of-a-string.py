5eadb2cea6dbc00fe998387c0440ddc1e3181ffc
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-04 12:15:43 +0530

    Time: 66 ms (64.79%), Space: 15.5 MB (61.09%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -4,16 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
-        
-        # method 1
-        pos=[]
-        s=list(s)
+        a=list(s)
         vowel={"a","e","i","o","u","A","E","I","O","U"}
-        for i in range(len(s)):
-            if s[i] in vowel:
-                pos.append(i)
-        pos_len=len(pos)
-        for i in range(pos_len//2):
-            s[pos[i]],s[pos[pos_len-1-i]]=s[pos[pos_len-1-i]],s[pos[i]]
-        return("".join(s))
+        i=0
+        j=len(a)-1
+        while(i<j):
+            if(a[i] not in vowel):
+                i+=1
+            if(a[j] not in vowel):
+                j-=1
+            if(a[i] in vowel and a[j] in vowel):
+                a[i],a[j]=a[j],a[i]
+                i+=1
+                j-=1
+        return("".join(a))
+        # method 1
+        
+        # pos=[]
+        # s=list(s)
+        # vowel={"a","e","i","o","u","A","E","I","O","U"}
+        # for i in range(len(s)):
+        #     if s[i] in vowel:
+        #         pos.append(i)
+        # pos_len=len(pos)
+        # for i in range(pos_len//2):
+        #     s[pos[i]],s[pos[pos_len-1-i]]=s[pos[pos_len-1-i]],s[pos[i]]
+        # return("".join(s))
