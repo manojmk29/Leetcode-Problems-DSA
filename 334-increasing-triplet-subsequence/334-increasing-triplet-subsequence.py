92f7017ac78a9c8aa85fee1aed7d93c4b5abf939
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-05 12:44:14 +0530

    Time: 1089 ms (7.20%), Space: 33.6 MB (23.73%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -4,20 +4,41 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        count=0
-        hmax={}
         l=len(nums)
         if(l<3):
             return(False)
-        hmax[l-1]=nums[l-1]
-        for i in range(l-2,-1,-1):
-            hmax[i]=max(nums[i],hmax[i+1])
-        hmax2={}
-        hmax2[0]=nums[0]
-        for i in range(1,l-1):
-            hmax2[i]=min(nums[i],hmax2[i-1])
-        curr=nums[0]
-        for i in range(1,l-1):
-            if(hmax[i]>nums[i] and hmax2[i]<nums[i]):
+        first=second=float("inf")
+        for i in range(0,l):
+            val=nums[i]
+            if(val<first):
+                first=val
+            if(val>first and val<second):
+                second=val
+            if(val>second):
                 return(True)
         return(False)
+        
+        
+        
+        
+        
+        
+        #time complexity o(n) sc o(n)
+        
+        # count=0
+        # hmax={}
+        # l=len(nums)
+        # if(l<3):
+        #     return(False)
+        # hmax[l-1]=nums[l-1]
+        # for i in range(l-2,-1,-1):
+        #     hmax[i]=max(nums[i],hmax[i+1])
+        # hmax2={}
+        # hmax2[0]=nums[0]
+        # for i in range(1,l-1):
+        #     hmax2[i]=min(nums[i],hmax2[i-1])
+        # curr=nums[0]
+        # for i in range(1,l-1):
+        #     if(hmax[i]>nums[i] and hmax2[i]<nums[i]):
+        #         return(True)
+        # return(False)
