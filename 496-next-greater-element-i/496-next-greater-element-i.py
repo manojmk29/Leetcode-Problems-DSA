6e089627e002c1c5894f25de32b9839f391ae705
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-06 21:58:12 +0530

    Time: 45 ms (65.65%), Space: 13.5 MB (96.81%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -5,19 +5,40 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        #method 2
+        hmap={}
+        stack=[]
+        n1=len(nums1)
+        n2=len(nums2)
+        for i in range(n2-1,-1,-1):
+            val=nums2[i]
+            while(stack and val>stack[-1]):
+                stack.pop()
+            if(stack):
+                hmap[val]=stack[-1]
+            else:
+                hmap[val]=-1
+            stack.append(val)
+        ret=[]
+        for i in nums1:
+            if i not in hmap:
+                ret.append(-1)
+            else:
+                ret.append(hmap[i])
+        return(ret)
         
         #method 1
-        
-        for i in range(len(nums1)):
-            x=nums1[i]
-            flag=0
-            maxi=-1
-            for j in range(len(nums2)):
-                if(nums2[j]==x):
-                    flag=1
-                if(flag==1):
-                    if(nums2[j]>x):
-                        maxi=nums2[j]
-                        break
-            nums1[i]=maxi    
-        return(nums1)
+        #O(n*m)
+        # for i in range(len(nums1)):
+        #     x=nums1[i]
+        #     flag=0
+        #     maxi=-1
+        #     for j in range(len(nums2)):
+        #         if(nums2[j]==x):
+        #             flag=1
+        #         if(flag==1):
+        #             if(nums2[j]>x):
+        #                 maxi=nums2[j]
+        #                 break
+        #     nums1[i]=maxi    
+        # return(nums1)
