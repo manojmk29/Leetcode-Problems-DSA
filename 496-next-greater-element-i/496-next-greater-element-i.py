2216335c3072177690d701da075de74c09feb88d
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-06 21:48:34 +0530

    Time: 290 ms (9.57%), Space: 13.6 MB (89.13%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums1)):
+            x=nums1[i]
+            flag=0
+            maxi=-1
+            for j in range(len(nums2)):
+                if(nums2[j]==x):
+                    flag=1
+                if(flag==1):
+                    if(nums2[j]>x):
+                        maxi=nums2[j]
+                        break
+            nums1[i]=maxi    
+        return(nums1)
