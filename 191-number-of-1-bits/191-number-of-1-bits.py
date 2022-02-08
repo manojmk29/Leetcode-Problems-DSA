a850a4876f8fdd3e8896473e6b73d10473bddc9f
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-08 19:54:37 +0530

    Time: 28 ms (38.84%), Space: 13.1 MB (99.78%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -4,17 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        cnt=0
+        a=n
+        while(a):
+            a&=a-1
+            cnt+=1
+        return(cnt)
         # return list(str(bin(n))).count('1')
         # a=int(str(n),2)
-        a=n
-        c=0
-        if(a==0):
-            nn=0
-        else:
-            nn=int(log(n,2))+1
-        while(nn):
-            if(a&1==1):
-                c+=1
-            a=a>>1
-            nn-=1
-        return(c)
+        
+        # a=n
+        # c=0
+        # if(a==0):
+        #     nn=0
+        # else:
+        #     nn=int(log(n,2))+1
+        # while(nn):
+        #     if(a&1==1):
+        #         c+=1
+        #     a=a>>1
+        #     nn-=1
+        # return(c)
