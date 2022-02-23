b16fea4928d9798c906ef5b1646a8b711a29900e
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-23 16:55:08 +0530

    Time: 400 ms (89.09%), Space: 14.6 MB (28.26%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -4,15 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        n=len(nums)
-        tgt=n-1
+        tgt=len(nums)-1
         temp=nums[0]
-        if(tgt==0):
-            return(True)
         for i in range(tgt):
-            if(nums[i]==0 and temp<=i):
+            if(temp<i):
                 return(False)
             temp=max(temp,i+nums[i])
             if(temp>=tgt):
                 return(True)
-        return(False)
+        return(temp>=tgt)
