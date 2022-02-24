6eef0d98dacf3e08062a6af3a097b89b70181dc1
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-02-24 08:43:04 +0530

    Time: 6392 ms (20.28%), Space: 14.5 MB (41.45%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -6,24 +6,39 @@ class Solution(object):
         """
         n=len(nums)
         dp=[-1 for i in range(n)]
-        for i in range(n):
-            if(nums[i]==0):
-                dp[i]=float("inf")
-        dp[-1]=0    
-        def helper(pos):
-            if(dp[pos]!=-1):
-                return(dp[pos])
-            v=nums[pos]+pos
-            dp[pos]=float("inf")
-            for i in range(v,pos,-1):
-                if(i>=n-1):
-                    dp[pos]=1
-                    break
+        dp[-1]=0
+        for i in range(n-2,-1,-1):
+            t=float("inf")
+            for j in range(i+nums[i],i,-1):
+                if(j>=n-1):
+                    v=0
                 else:
-                    if(dp[i]==-1):
-                        helper(i)
-                    dp[pos]=min(dp[pos],1+dp[i])
-            return(dp[pos])
-        helper(0)
+                    v=dp[j]
+                t=min(t,v+1)
+            dp[i]=t
         return(dp[0])
+                
+        
+        # n=len(nums)
+        # dp=[-1 for i in range(n)]
+        # for i in range(n):
+        #     if(nums[i]==0):
+        #         dp[i]=float("inf") 
+        # dp[-1]=0
+        # def helper(pos):
+        #     if(dp[pos]!=-1):
+        #         return(dp[pos])
+        #     v=nums[pos]+pos
+        #     dp[pos]=float("inf")
+        #     for i in range(v,pos,-1):
+        #         if(i>=n-1):
+        #             dp[pos]=1
+        #             break
+        #         else:
+        #             if(dp[i]==-1):
+        #                 helper(i)
+        #             dp[pos]=min(dp[pos],1+dp[i])
+        #     return(dp[pos])
+        # helper(0)
+        # return(dp[0])
                         
