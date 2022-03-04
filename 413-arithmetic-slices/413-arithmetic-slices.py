651de691afe6e42375d9705329014a6a67facb01
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-03-04 09:56:51 +0530

    Time: 20 ms (96.00%), Space: 13.5 MB (95.69%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,19 +1,17 @@
-class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        @lru_cache
-        def count(n):
-            if cnt < 3: return 0
-            return n - 2 + count(n - 1)
-        
-        prev, cnt, ans = None, 2, 0
+class Solution(object):
+    def numberOfArithmeticSlices(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prev, cnt, ans = None, 0, 0
         for i in range(1, len(nums)):
             diff = nums[i-1] - nums[i]
             if diff == prev:
                 cnt += 1
+                if(cnt>=3):
+                    ans+=(cnt-2)
             else:
-                ans += count(cnt)
                 cnt = 2
-                
             prev = diff
-            
-        return ans + count(cnt)
+        return(ans)
