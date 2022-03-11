0d5bbb20bb5fba321305c6be4276ae93222a52c9
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-03-11 09:28:30 +0530

    Time: 108 ms (37.91%), Space: 13.9 MB (28.02%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
@@ -5,26 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        le=len(s)
-        def helper(b,e):
-            if(b<0 or e>=le):
-                return(0)
-            hmap=collections.defaultdict(int)
-            se={}
-            for i in range(b,e+1):
-                hmap[s[i]]+=1
-                if s[i] not in se:
-                    se[s[i]]=[i,i]
-                else:
-                    se[s[i]][1]=i
-            l=float("inf")
-            r=-l
-            for i in hmap:
-                if(hmap[i]<k):
-                    l=min(l,se[i][0])
-                    r=max(r,se[i][1])
-            if(l==float("inf")):
-                return(e-b+1)
-            else:
-                return(max(helper(b,l-1),helper(l+1,e)))
-        return(helper(0,le-1))
+        hset=len(set(s))
+        ret=0
+        for i in range(1,hset+1):
+            unique=0
+            hmap=defaultdict(int)
+            l=0
+            sle=len(s)
+            kk=set()
+            for r in range(sle):
+                val=s[r]
+                hmap[val]+=1
+                if(hmap[val]==1):
+                    unique+=1
+                if(hmap[val]==k):
+                    kk.add(val)
+                while(unique>i):
+                    hmap[s[l]]-=1
+                    if(hmap[s[l]]==0):
+                        unique-=1
+                    if(s[l] in kk and hmap[s[l]]<k):
+                        kk.remove(s[l])
+                    l+=1
+                if(len(kk)==unique):
+                    ret=max(ret,r-l+1)
+        return(ret)        
