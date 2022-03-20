f99836474ed973b59f3304fcd56ed85ec52e2101
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-03-20 17:36:55 +0530

    Time: 41 ms (12.84%), Space: 13.3 MB (72.54%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -6,24 +6,23 @@ class Solution(object):
         """
         m=len(board)
         n=len(board[0])
-        flag=[[0 for i in range(n)]for i in range(m)]
         for i in range(m):
             for j in range(n):
                 cnt=0
                 for x in(-1,0,1):
                     for y in(-1,-0,1):
                         if((0<=i+x<=m-1) and (0<=j+y<=n-1)):
-                            cnt+=board[i+x][j+y] 
+                                cnt+=board[i+x][j+y]%2
                 cnt-=board[i][j]
-                flag[i][j]=cnt
+                if(board[i][j]==1):
+                    if(cnt<2 or cnt>3):
+                        board[i][j]=3
+                elif(cnt==3):
+                    board[i][j]=2
         for i in range(m):
             for j in range(n):
-                val=board[i][j]
-                if(val==1):
-                    if(flag[i][j]<2):
-                        board[i][j]=0
-                    elif(flag[i][j]>3):
-                        board[i][j]=0
-                elif(flag[i][j]==3):
+                if(board[i][j]==3):
+                    board[i][j]=0
+                elif(board[i][j]==2):
                     board[i][j]=1
             
