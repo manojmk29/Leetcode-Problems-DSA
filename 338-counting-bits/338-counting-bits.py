c989a6f6d2ab3c3a9686852bbddc8708a37bcb76
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-03-21 11:30:00 +0530

    Time: 102 ms (50.55%), Space: 17.3 MB (75.50%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -4,19 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        ret=[0]
-        ptr=0
-        flag=2
-        for i in range(n):
-            val=ret[ptr]
-            if(flag==1):
-                ret.append(val)
-                flag+=1
-            elif(flag==2 or flag==3):
-                ret.append(val+1)
-                flag+=1
-            elif(flag==4):
-                ret.append(val+2)
-                ptr+=1
-                flag=1
+        ret=[0 for i in range(n+1)]
+        if(not n):
+            return(ret)
+        ret[1]=1
+        for i in range(2,n+1):
+            if(i&1==1):
+                ret[i]=ret[i//2]+1
+            else:
+                ret[i]=ret[i//2]
         return(ret)
+            
