cb46de2a904699ecf174e101d8b967e027942b14
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-03-21 09:47:08 +0530

    Time: 32 ms (72.74%), Space: 13.3 MB (96.93%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -5,15 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
         hmap={}
-        for i,j in enumerate(s):
-            hmap[j]=i
         ret=[]
-        l=r=0
-        for ind,val in enumerate(s):
-            r=max(r,hmap[val])
-            if(ind==r):
-                le=r-l+1
-                ret.append(le)
-                l=r+1
+        for i in range(len(s)):
+            hmap[s[i]]=i
+        last=-1
+        prev=0
+        for i in range(len(s)):
+            last=max(last,hmap[s[i]])
+            if(last==i):
+                ret.append(i+1-prev)
+                prev=i+1
+                last=-1
         return(ret)
-                
+            
