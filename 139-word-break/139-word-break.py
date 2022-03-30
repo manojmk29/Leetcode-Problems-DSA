9727521234960bd858505eca6f2ee0e9725d8d65
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-03-30 20:52:37 +0530

    Time: 44 ms (80.55%), Space: 14.2 MB (28.59%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,13 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordDict=set(wordDict)
-        @lru_cache
+        hmap={}
         def helper(word):
+            if word in hmap:
+                return(hmap[word])
             if(len(word)==0):
+                hmap[word]=True
                 return(True)
             for i in range(len(word),0,-1):
                 if((word[0:i] in wordDict) and (helper(word[i:]) == True ) ):
+                    hmap[word]=True
                     return(True)
+            hmap[word]=False
             return(False)
         return(helper(s))
         
