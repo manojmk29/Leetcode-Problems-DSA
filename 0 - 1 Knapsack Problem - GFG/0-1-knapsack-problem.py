326625aee415a46edcd2a1cdb2457f6bb39e1990
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-03 22:00:02 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -2,20 +2,27 @@
 from functools import lru_cache
 class Solution:
     def knapSack(self,W, wt, val, n):
-        @lru_cache(None)
+        # @lru_cache(None)
+        dp=[[-1 for i in range(W+1)] for i in range(n)]
         def helper(ind,w):
+            if(dp[ind][w]!=-1):
+                return(dp[ind][w])
             if(w==0):
+                dp[ind][w]=0
                 return(0)
             if(ind==0):
                 if(wt[0]<=w):
+                    dp[ind][w]=val[0]
                     return(val[0])
                 else:
+                    dp[ind][w]=0
                     return(0)
             take=-float("inf")
             notake=helper(ind-1,w)
             if(wt[ind]<=w):
                 take=val[ind]+helper(ind-1,w-wt[ind])
-            return(max(take,notake))
+            dp[ind][w]=max(take,notake)
+            return(dp[ind][w])
         return(helper(n-1,W))
         # code here
 
