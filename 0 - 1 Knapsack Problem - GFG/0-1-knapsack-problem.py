a3305dc91eaf76bddd333c3e2d891bb92d202096
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-04 15:23:15 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -1,29 +1,19 @@
 #User function Template for python3
-from functools import lru_cache
 class Solution:
     def knapSack(self,W, wt, val, n):
-        # @lru_cache(None)
-        dp=[[-1 for i in range(W+1)] for i in range(n)]
-        def helper(ind,w):
-            if(dp[ind][w]!=-1):
-                return(dp[ind][w])
-            if(w==0):
-                dp[ind][w]=0
-                return(0)
-            if(ind==0):
-                if(wt[0]<=w):
-                    dp[ind][w]=val[0]
-                    return(val[0])
-                else:
-                    dp[ind][w]=0
-                    return(0)
-            take=-float("inf")
-            notake=helper(ind-1,w)
-            if(wt[ind]<=w):
-                take=val[ind]+helper(ind-1,w-wt[ind])
-            dp[ind][w]=max(take,notake)
-            return(dp[ind][w])
-        return(helper(n-1,W))
+        dp=[0 for i in range(W+1)]
+        for i in range(wt[0],W+1):
+            dp[i]=val[0]
+        for ind in range(1,n):
+            next=[0 for i in range(W+1)]
+            for weit in range(0,W+1):
+                notTaken =dp[weit];
+                taken = -float("inf");
+                if(wt[ind] <= weit):
+                    taken = val[ind] + dp[weit - wt[ind]]
+                next[weit] = max(notTaken, taken)
+            dp=next
+        return(dp[W])
         # code here
 
 #{ 
