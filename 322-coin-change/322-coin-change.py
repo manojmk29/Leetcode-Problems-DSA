1545a448995dc8e8a1c40c5ea547980dcc00f03f
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-05 18:45:45 +0530

    Time: 2832 ms (15.72%), Space: 23.7 MB (15.01%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,7 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
+        dp=[-1 for i in range(amount+1)]
+        dp[0]=0
         def helper(tot):
+            if(dp[tot]!=-1):
+                return(dp[tot])
             if(tot==0):
                 return(0)
             if(tot<0):
@@ -10,6 +13,7 @@ class Solution:
             for i in coins:
                 if(i<=tot):
                     ret=min(ret,1+helper(tot-i))
+            dp[tot]=ret
             return(ret)
         val=helper(amount)
         return(val if val!=float("inf") else -1)
