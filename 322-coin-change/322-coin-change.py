0a540f15d914bdc1014f03e08fd99474c48bcee1
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-05 19:21:46 +0530

    Time: 2188 ms (39.21%), Space: 36.8 MB (8.38%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,19 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[-1 for i in range(amount+1)]
-        dp[0]=0
-        def helper(tot):
-            if(dp[tot]!=-1):
-                return(dp[tot])
-            if(tot==0):
-                return(0)
-            if(tot<0):
-                return(float("inf"))
-            ret=float("inf")
-            for i in coins:
-                if(i<=tot):
-                    ret=min(ret,1+helper(tot-i))
-            dp[tot]=ret
+        dp=[[-1 for i in range(amount+1)]for _ in range(len(coins))]
+        def helper(ind,tot):
+            if(ind==0):
+                if(tot%coins[0]==0):
+                    return(tot//coins[0])
+                else:
+                    return(float("inf"))
+            if(dp[ind][tot]!=-1):
+                return(dp[ind][tot])
+            notake=helper(ind-1,tot)
+            take=float("inf")
+            if(coins[ind]<=tot):
+                take=1+helper(ind,tot-coins[ind])
+            ret=min(notake,take)
+            dp[ind][tot]=ret
             return(ret)
-        val=helper(amount)
+        val=helper(len(coins)-1,amount)
         return(val if val!=float("inf") else -1)
