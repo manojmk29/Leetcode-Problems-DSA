a83e000310b27c4885981a98ac80f2b7da0bf0f3
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-06 10:21:48 +0530

    Time: 2576 ms (22.00%), Space: 18 MB (23.51%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,20 +1,22 @@
+"""
+Here we are using memo technique.
+
+"""
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[[-1 for i in range(amount+1)]for _ in range(len(coins))]
-        def helper(ind,tot):
-            if(ind==0):
-                if(tot%coins[0]==0):
-                    return(tot//coins[0])
-                else:
-                    return(float("inf"))
-            if(dp[ind][tot]!=-1):
-                return(dp[ind][tot])
-            notake=helper(ind-1,tot)
-            take=float("inf")
-            if(coins[ind]<=tot):
-                take=1+helper(ind,tot-coins[ind])
-            ret=min(notake,take)
-            dp[ind][tot]=ret
-            return(ret)
-        val=helper(len(coins)-1,amount)
+    def coinChange(self, coins, amount: int) -> int:
+        n=len(coins)
+        dp=[[0 for i in range(amount+1)]for _ in range(n)]
+        for i in range(1,amount+1):
+            if(i%coins[0]==0):
+                dp[0][i]=i//coins[0]
+            else:
+                dp[0][i]=float("inf")
+        for ind in range(1,len(coins)):
+            for val in range(1,amount+1):
+                notake=dp[ind-1][val]
+                take=float("inf")
+                if(coins[ind]<=val):
+                    take=1+dp[ind][val-coins[ind]]
+                dp[ind][val]=min(take,notake)
+        val=dp[n-1][amount]
         return(val if val!=float("inf") else -1)
