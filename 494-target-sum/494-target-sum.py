24a8227b84bf12195ff672e3c2843e5a4f216f1c
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-06 14:33:20 +0530

    Time: 180 ms (93.32%), Space: 15.8 MB (49.10%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -14,18 +14,24 @@ class Solution:
         tot+=target
         if(tot%2!=0):
             return(0)
-        tar=tot//2
-        @lru_cache(None)
+        tar=abs(tot//2)
+        n=len(nums)
+        dp=[[-1 for i in range(tar+1)]for i in range(n)]
         def helper(ind,cur):
+            if(dp[ind][cur]!=-1):
+                return(dp[ind][cur])
             if(ind==0):
                 if(cur==0 or cur-nums[ind]==0):
+                    dp[ind][cur]=1+(nums[ind]==0)
                     return(1+(nums[ind]==0))
                 else:
+                    dp[ind][cur]=0
                     return(0)
             notake=helper(ind-1,cur)
             take=0
-            if(cur>=0):
+            if(cur-nums[ind]>=0):
                 take=helper(ind-1,cur-nums[ind])
+            dp[ind][cur]=notake+take
             return(notake+take)
         n=len(nums)
         return(helper(n-1,tar))
