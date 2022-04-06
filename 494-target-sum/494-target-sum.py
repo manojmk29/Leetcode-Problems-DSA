832d5ac45151207a875fccd106250d6fcd88df1f
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-06 14:50:58 +0530

    Time: 220 ms (89.31%), Space: 14.2 MB (73.52%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -14,24 +14,20 @@ class Solution:
         tot+=target
         if(tot%2!=0):
             return(0)
-        tar=abs(tot//2)
+        tar=tot//2
+        tar=abs(tar)
         n=len(nums)
         dp=[[-1 for i in range(tar+1)]for i in range(n)]
-        def helper(ind,cur):
-            if(dp[ind][cur]!=-1):
-                return(dp[ind][cur])
-            if(ind==0):
-                if(cur==0 or cur-nums[ind]==0):
-                    dp[ind][cur]=1+(nums[ind]==0)
-                    return(1+(nums[ind]==0))
-                else:
-                    dp[ind][cur]=0
-                    return(0)
-            notake=helper(ind-1,cur)
-            take=0
-            if(cur-nums[ind]>=0):
-                take=helper(ind-1,cur-nums[ind])
-            dp[ind][cur]=notake+take
-            return(notake+take)
-        n=len(nums)
-        return(helper(n-1,tar))
+        for cur in range(tar+1):
+            if(cur==0 or cur-nums[0]==0):
+                dp[0][cur]=1+(nums[0]==0)
+            else:
+                dp[0][cur]=0
+        for ind in range(1,n):
+            for cur in range(tar+1):
+                notake=dp[ind-1][cur]
+                take=0
+                if(cur>=nums[ind]):
+                    take=dp[ind-1][cur-nums[ind]]
+                dp[ind][cur]=notake+take
+        return(dp[n-1][tar])
