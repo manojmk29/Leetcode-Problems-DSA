458290502f2550c03ae14cf591c398d271e84a6a
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-07 14:27:33 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -1,4 +1,30 @@
-#User function Template for python3
+from functools import lru_cache
+class Solution:
+    def cutRod(self, price, n):
+        dp=[[0 for i in range(n+1)]for i in range(n)]
+        for i in range(n+1):
+            dp[0][i]=i*price[0]
+        for ind in range(1,n):
+            for val in range(n+1):
+                notake=dp[ind-1][val]
+                take=-float("inf")
+                if(ind+1<=val):
+                    take=price[ind]+dp[ind][val-ind-1]
+                dp[ind][val]=max(take,notake)
+        return(dp[n-1][n])
+                
+        
+        # @lru_cache(None)
+        # def helper(ind,val):
+        #     if(ind==0):
+        #         return(val*price[ind])
+        #     notake=helper(ind-1,val)
+        #     take=-float("inf")
+        #     if(ind+1<=val):
+        #         take=price[ind]+helper(ind,val-ind-1)
+        #     return(max(take,notake))
+        # return(helper(n-1,n))
+            
 # from functools import lru_cache
 # class Solution:
 #     def cutRod(self, price, n):
@@ -8,25 +34,11 @@
 #                 return(val*price[ind])
 #             notake=helper(ind-1,val)
 #             take=-float("inf")
-#             if(ind+1<=val):
-#                 take=price[ind]+helper(ind,val-ind+1)
+#             rodLength = ind+1
+#             if(rodLength<=val):
+#                 take=price[ind]+helper(ind,val-rodLength)
 #             return(max(take,notake))
-#         return(helper(n-1,n))
-            
-from functools import lru_cache
-class Solution:
-    def cutRod(self, price, n):
-        @lru_cache(None)
-        def helper(ind,val):
-            if(ind==0):
-                return(val*price[ind])
-            notake=helper(ind-1,val)
-            take=-float("inf")
-            rodLength = ind+1
-            if(rodLength<=val):
-                take=price[ind]+helper(ind,val-rodLength)
-            return(max(take,notake))
-        return(helper(n-1,n))           
+#         return(helper(n-1,n))           
 
 #{ 
 #  Driver Code Starts
