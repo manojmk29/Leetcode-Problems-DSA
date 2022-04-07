8c789a7b26153d76efb9cbf7b55dc7db2035a53f
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-07 12:41:13 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -1,4 +1,18 @@
 #User function Template for python3
+# from functools import lru_cache
+# class Solution:
+#     def cutRod(self, price, n):
+#         @lru_cache(None)
+#         def helper(ind,val):
+#             if(ind==0):
+#                 return(val*price[ind])
+#             notake=helper(ind-1,val)
+#             take=-float("inf")
+#             if(ind+1<=val):
+#                 take=price[ind]+helper(ind,val-ind+1)
+#             return(max(take,notake))
+#         return(helper(n-1,n))
+            
 from functools import lru_cache
 class Solution:
     def cutRod(self, price, n):
@@ -12,9 +26,7 @@ class Solution:
             if(rodLength<=val):
                 take=price[ind]+helper(ind,val-rodLength)
             return(max(take,notake))
-        return(helper(n-1,n))
-            
-            
+        return(helper(n-1,n))           
 
 #{ 
 #  Driver Code Starts
