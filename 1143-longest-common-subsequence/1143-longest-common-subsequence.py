3ceb0d7043a659d0d770d05850f78ed6bc2b005c
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-19 01:27:57 +0530

    Time: 1211 ms (17.32%), Space: 140 MB (17.57%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,16 +3,35 @@ class Solution:
         m=len(text1)
         n=len(text2)
         @lru_cache(None)
-        def helper(ai,bi):
+        def helper(a,b):
             ret=left=right=0
-            if(text1[ai]==text2[bi]):
+            if(text1[a]==text2[b]):
                 ret=1
-                if(ai+1<=m-1 and bi+1<=n-1):
-                    ret+=helper(ai+1,bi+1)
-            else:
-                if(ai+1<=m-1):
-                    left=helper(ai+1,bi)
-                if(bi+1<=n-1):
-                    right=helper(ai,bi+1)   
-            return(ret+max(left,right))
-        return(helper(0,0))
+                if(a-1>=0 and b-1>=0):
+                    ret+=helper(a-1,b-1)
+                return(ret)
+            if(a-1>=0):
+                left=helper(a-1,b)
+            if(b-1>=0):
+                right=helper(a,b-1)
+            val=max(left,right)
+            return(val)
+        return(helper(m-1,n-1))
+            
+            
+#         m=len(text1)
+#         n=len(text2)
+#         @lru_cache(None)
+#         def helper(ai,bi):
+#             ret=left=right=0
+#             if(text1[ai]==text2[bi]):
+#                 ret=1
+#                 if(ai+1<=m-1 and bi+1<=n-1):
+#                     ret+=helper(ai+1,bi+1)
+#             else:
+#                 if(ai+1<=m-1):
+#                     left=helper(ai+1,bi)
+#                 if(bi+1<=n-1):
+#                     right=helper(ai,bi+1)   
+#             return(ret+max(left,right))
+#         return(helper(0,0))
