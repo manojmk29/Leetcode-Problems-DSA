c17b0540c51ebc118d273d0373f830b1b98e7860
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-19 08:46:45 +0530

    Time: 382 ms (94.61%), Space: 22.7 MB (44.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,40 +2,22 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         m=len(text1)
         n=len(text2)
-        dp=[[-1 for i in range(n)]for i in range(m)]
-        def helper(a,b):
-            ret=left=right=0
-            if(dp[a][b]!=-1):
-                return(dp[a][b])
-            if(text1[a]==text2[b]):
-                ret=1
-                if(a-1>=0 and b-1>=0):
-                    ret+=helper(a-1,b-1)
-                    dp[a][b]=ret
-                return(ret)
-            if(a-1>=0):
-                left=helper(a-1,b)
-            if(b-1>=0):
-                right=helper(a,b-1)
-            val=max(left,right)
-            dp[a][b]=val
-            return(val)
-        return(helper(m-1,n-1))
-            
-            
-#         m=len(text1)
-#         n=len(text2)
-#         @lru_cache(None)
-#         def helper(ai,bi):
-#             ret=left=right=0
-#             if(text1[ai]==text2[bi]):
-#                 ret=1
-#                 if(ai+1<=m-1 and bi+1<=n-1):
-#                     ret+=helper(ai+1,bi+1)
-#             else:
-#                 if(ai+1<=m-1):
-#                     left=helper(ai+1,bi)
-#                 if(bi+1<=n-1):
-#                     right=helper(ai,bi+1)   
-#             return(ret+max(left,right))
-#         return(helper(0,0))
+        dp=[[0 for i in range(n)]for i in range(m)]
+        dp[0][0]=int(text2[0]==text1[0])
+        for i in range(1,m):
+            if(text2[0]==text1[i]):
+                dp[i][0]=1
+            else:
+                dp[i][0]=dp[i-1][0]
+        for i in range(1,n):
+            if(text1[0]==text2[i]):
+                dp[0][i]=1
+            else:
+                dp[0][i]=dp[0][i-1]
+        for i in range(1,m):
+            for j in range(1,n):
+                if(text1[i]==text2[j]):
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return(dp[m-1][n-1])
