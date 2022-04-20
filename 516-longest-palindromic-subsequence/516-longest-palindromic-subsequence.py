99fcd7434367f589886052cb4459cd4bfbd6607e
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-20 14:47:03 +0530

    Time: 2581 ms (42.26%), Space: 14 MB (95.01%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -3,17 +3,29 @@ class Solution:
             text1=s
             text2=s[::-1]
             m=n=len(text1)
-            @lru_cache(None)
-            def helper(ai,bi):
-                ret=left=right=0
-                if(text1[ai]==text2[bi]):
-                    ret=1
-                    if(ai+1<=m-1 and bi+1<=n-1):
-                        ret+=helper(ai+1,bi+1)
-                else:
-                    if(ai+1<=m-1):
-                        left=helper(ai+1,bi)
-                    if(bi+1<=n-1):
-                        right=helper(ai,bi+1)   
-                return(ret+max(left,right))
-            return(helper(0,0))
+            prev=[0 for i in range(n+1)]
+            cur=[0 for i in range(n+1)]
+            for i in range(1,m+1):
+                for j in range(1,n+1):
+                    if(text1[i-1]==text2[j-1]):
+                        cur[j]=1+prev[j-1]
+                    else:
+                        cur[j]=max(prev[j],cur[j-1])
+                prev=cur[:]
+            return(prev[n])
+        
+#             @lru_cache(None)
+#             def helper(ai,bi):
+#                 ret=left=right=0
+#                 if(text1[ai]==text2[bi]):
+#                     ret=1
+#                     if(ai+1<=m-1 and bi+1<=n-1):
+#                         ret+=helper(ai+1,bi+1)
+#                 else:
+#                     if(ai+1<=m-1):
+#                         left=helper(ai+1,bi)
+#                     if(bi+1<=n-1):
+#                         right=helper(ai,bi+1)   
+#                 return(ret+max(left,right))
+#             return(helper(0,0))
+        
