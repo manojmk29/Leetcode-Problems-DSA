52efeee1d1f7a3a16e13ed9ad56012270b324736
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-28 18:23:31 +0530

    Time: 52 ms (85.62%), Space: 14.1 MB (96.00%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,44 +1,17 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        #method 2
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hmap={}
-        stack=[]
-        n1=len(nums1)
-        n2=len(nums2)
-        for i in range(n2-1,-1,-1):
-            val=nums2[i]
-            while(stack and val>stack[-1]):
-                stack.pop()
-            if(stack):
-                hmap[val]=stack[-1]
+        stk=[]
+        for i in range(len(nums2)-1,-1,-1):
+            while(stk and stk[-1]<=nums2[i]):
+                stk.pop()
+            if(not stk):
+                hmap[nums2[i]]=-1
             else:
-                hmap[val]=-1
-            stack.append(val)
+                hmap[nums2[i]]=stk[-1]
+            stk.append(nums2[i])
         ret=[]
         for i in nums1:
-            if i not in hmap:
-                ret.append(-1)
-            else:
-                ret.append(hmap[i])
+            ret.append(hmap[i])
         return(ret)
         
-        #method 1
-        #O(n*m)
-        # for i in range(len(nums1)):
-        #     x=nums1[i]
-        #     flag=0
-        #     maxi=-1
-        #     for j in range(len(nums2)):
-        #         if(nums2[j]==x):
-        #             flag=1
-        #         if(flag==1):
-        #             if(nums2[j]>x):
-        #                 maxi=nums2[j]
-        #                 break
-        #     nums1[i]=maxi    
-        # return(nums1)
