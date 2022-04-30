85303a802f8d9d44b088b21a3a34cdc8d2e04c8f
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-04-30 10:33:35 +0530

    Time: 47 ms (45.87%), Space: 13.9 MB (45.81%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -4,38 +4,14 @@ class Solution:
         n=len(s)
         dp=[-1 for i in range(n+1)]
         dp[n]=1
-        def helper(ind):
-            if(dp[ind]!=-1):
-                return(dp[ind])
+        for ind in range(n-1,-1,-1):
             if(s[ind]=="0"):
                 dp[ind]=0
-                return(0)
-            tot=0
-            tot+=helper(ind+1)
-            if(ind+1<n):
-                if(s[ind]=="1" or ( s[ind]=="2" and s[ind+1]<"7")):
-                    tot+=helper(ind+2)
-            dp[ind]=tot
-            return(tot)
-        return(helper(0))
-    
-    
-
-
-# from functools import lru_cache
-# class Solution:
-#     @lru_cache
-#     def numDecodings(self, s: str) -> int:
-#         n=len(s)
-#         def helper(ind):
-#             if(ind==n):
-#                 return(1)
-#             if(s[ind]=="0"):
-#                 return(0)
-#             tot=0
-#             tot+=helper(ind+1)
-#             if(ind+1<n):
-#                 if(s[ind]=="1" or ( s[ind]=="2" and s[ind+1]<"7")):
-#                     tot+=helper(ind+2)
-#             return(tot)
-#         return(helper(0))
+            else:
+                tot=0
+                tot+=dp[ind+1]
+                if(ind+1<n):
+                    if(s[ind]=="1" or ( s[ind]=="2" and s[ind+1]<"7")):
+                        tot+=dp[ind+2]
+                dp[ind]=tot
+        return(dp[0])
