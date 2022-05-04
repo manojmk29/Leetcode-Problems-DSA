41bfd46a5056eaf7cc4a30a5f48e4003a5707ec4
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-05-04 11:04:11 +0530

    Time: 1229 ms (88.07%), Space: 25 MB (50.38%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,18 +1,30 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stk=[]
-        ret=[]
-        hmap={}
         n=len(temperatures)
-        for i in range(n-1,-1,-1):
-            cnt=1
-            cur=temperatures[i]
-            while(stk and stk[-1][1]<=cur):
-                cnt+=1
-                cnt+=hmap[stk[-1][0]]
-                stk.pop()
-            hmap[i]=cnt-1
-            cnt=cnt if(stk) else 0
-            ret.append(cnt)
-            stk.append((i,cur))
-        return(ret[::-1])
+        ans=[0]*n
+        stk=[]
+        for i in range(n):
+            while(stk and temperatures[stk[-1]]<temperatures[i]):
+                popped=stk.pop()
+                ans[popped]=i-popped
+            stk.append(i)
+        return(ans)
+        
+        
+        
+        # stk=[]
+        # ret=[]
+        # hmap={}
+        # n=len(temperatures)
+        # for i in range(n-1,-1,-1):
+        #     cnt=1
+        #     cur=temperatures[i]
+        #     while(stk and stk[-1][1]<=cur):
+        #         cnt+=1
+        #         cnt+=hmap[stk[-1][0]]
+        #         stk.pop()
+        #     hmap[i]=cnt-1
+        #     cnt=cnt if(stk) else 0
+        #     ret.append(cnt)
+        #     stk.append((i,cur))
+        # return(ret[::-1])
