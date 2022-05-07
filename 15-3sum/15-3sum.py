4a58cb4fad7e1cafb12271015ed191fe4ece2f04
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-05-07 12:32:31 +0530

    Time: 1819 ms (34.09%), Space: 18.1 MB (42.65%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,25 +1,29 @@
-class Solution(object):
+class Solution:
     def threeSum(self, nums):
-        res = []
+        n=len(nums)
         nums.sort()
-        for i in range(0,len(nums)-2):
-            if(i>0 and nums[i-1]==nums[i]):
-                continue
+        result=[]
+        i=0
+        while(i<n-2):
             l=i+1
-            r=len(nums)-1
-            while(l<r):
-                s=nums[i]+nums[l]+nums[r]
-                if(s>0):
-                    r-=1
-                elif(s<0):
-                    l+=1
-                else:
-                    res.append([nums[i],nums[l],nums[r]])
-                    while(l<len(nums)-1 and nums[l]==nums[l+1]):
-                        l+=1
-                    while(r>0 and nums[r]==nums[r-1]):
-                        r-=1
-                    l=l+1
+            r=n-1
+            while (l<r):
+                total=nums[i]+nums[l]+nums[r]
+                if total>0:
                     r=r-1
-        return(res)
-                    
+                elif total<0:
+                    l=l+1
+                else:
+                    result.append([nums[i],nums[l],nums[r]])
+                    while(l+1<n and nums[l+1]==nums[l]):
+                        l+=1
+                    l+=1
+                    while(r-1>=0 and nums[r-1]==nums[r]):
+                        r-=1
+                    r-=1
+            while(i+1<n and nums[i]==nums[i+1]):
+                i+=1
+            i+=1
+        return(result)
+                        
+        
