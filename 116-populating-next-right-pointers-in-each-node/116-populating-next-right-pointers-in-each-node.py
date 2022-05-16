b2dae88e74b2800b2b13f27635d963c26c19b5f0
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-05-16 09:44:23 +0530

    Time: 119 ms (15.27%), Space: 15.6 MB (75.79%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -1,31 +1,13 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
+from collections import deque
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        stk=deque([])
-        if(root):
-            stk.append(root)
-        l=0
-        while(stk):
-            prev=None
-            n=len(stk)
-            while(n):
-                if(prev):
-                    prev.next=stk[0]
-                prev=stk.popleft()
-                if(prev.left):
-                    stk.append(prev.left)
-                if(prev.right):
-                    stk.append(prev.right)
-                n-=1
-        return(root)
-                
-        
+    def connect(self, root):
+        def helper(node):
+            if(node):
+                if(node.left):
+                    node.left.next=node.right
+                    if(node.next):
+                        node.right.next=node.next.left
+                helper(node.left)
+                helper(node.right)
+            return(node)
+        return(helper(root))
