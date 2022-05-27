91226eb405f667e90dc3a5b9265dfa52c8b7d9fe
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-05-27 18:42:39 +0530

    Time: 47 ms (53.35%), Space: 13.8 MB (74.63%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,15 +1,23 @@
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        n=len(matrix[0])
-        for i in range(n):
-            for j in range(n):
+        """
+        00 01 02
+        10 11 12
+        20 21 22
+        
+        00 10 20
+        01 11 21
+        02 12 22
+        """
+        
+        m=len(matrix)
+        for i in range(m):
+            for j in range(m):
                 if(i<j):
                     matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-        for i in range(n):
-            for j in range(n//2):
-                matrix[i][j],matrix[i][n-j-1]=matrix[i][n-j-1],matrix[i][j]
-        return(matrix)
+        for i in range(m-1//2):
+            for j in range(m//2):
+                matrix[i][j],matrix[i][m-j-1]=matrix[i][m-j-1],matrix[i][j]
