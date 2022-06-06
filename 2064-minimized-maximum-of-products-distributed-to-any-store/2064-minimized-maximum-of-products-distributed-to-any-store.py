4267e9a9b88a4f7cc9cc68cdba2888f533bfe49d
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2022-06-06 15:08:00 +0530

    Time: 3121 ms (37.98%), Space: 28.2 MB (79.33%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimizedMaximum(self, n, q) -> int:
+        lent=len(q)
+        def check(val):
+            cnt=0
+            for i in range(lent):
+                cnt+=ceil(q[i]/val)
+            return(cnt)
+        l=1
+        r=max(q)
+        while(l<r):
+            mid=(l+r)//2
+            c=check(mid)
+            if(c>n):
+                l=mid+1
+            else:
+                ret=mid
+                r=mid
+        return(l)       
